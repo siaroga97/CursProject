acbe0f6eb12dd57b6f96f27f6bc4819e9f976772
Sergey <sergey_mikiyanec@mail.ru>
2017-12-13 02:33:33 +0300

    refactor code

--- app/src/main/java/com/example/notepadby/cursproject/DetailsActivity.java
+++ app/src/main/java/com/example/notepadby/cursproject/DetailsActivity.java
@@ -9,9 +9,8 @@ import com.example.notepadby.cursproject.constants.Constants;
 import com.example.notepadby.cursproject.entity.ListElement;
 
 
-
 public class DetailsActivity extends Activity {
-    private EventDbHelper eventDbHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,17 +21,10 @@ public class DetailsActivity extends Activity {
         TextView descriptionView = findViewById(R.id.detailDescription);
         TextView dateView = findViewById(R.id.detailDate);
         Integer id = getIntent().getIntExtra(Constants.INTENT_ID, 0);
-        ListElement element=new ListElement();
-        for(ListElement listElement:Constants.globalList){
-            if(id==listElement.getId()){
-                 element =listElement;
-                break;
-            }
-        }
-
+        ListElement element = Constants.globalList.get(id-1);
 
         titleView.setText(element.getTitle());
         descriptionView.setText(element.getDescription());
-        dateView.setText(android.text.format.DateFormat.format("EEE. dd  MMMM yyyy HH:mm",element.getDate()));
+        dateView.setText(android.text.format.DateFormat.format("EEE. dd  MMMM yyyy HH:mm", element.getDate()));
     }
 }
